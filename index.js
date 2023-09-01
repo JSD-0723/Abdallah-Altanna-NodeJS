cf4f9a9499d0028a2e4df6fe9acafaff1623c492
abdallahaltanna <abdallahmyaltanna@gmail.com>
2023-09-01 23:04:35 +0300

    Edit Some Details

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import http from 'http';
 import fs from 'fs/promises';
-import { format } from 'date-fns';
 
 const server = http.createServer(async (req, res) => {
   if (req.url !== '/favicon.ico') {
-    const currentTime = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    const currentTime = new Date();
     const requestUrl = req.url;
 
     const logMessage = `${currentTime} - ${requestUrl}\n`;
